777aee9ef269750a3af344df5c4564b179dd3fae
shuiguang <zhengshuiguang@163.com>
2015-12-04 16:14:37 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,54 +1,60 @@
 <?php
-set_time_limit(0);
-header("Content-type:text/html;charset=utf-8");
 include __DIR__ .'/db.class.php';
-for($i=0; $i<1; $i++)
-{
-    p($db_config['driver']);
-    $db_config['database'] = 'test';
-    $db = new DB($db_config);
-    $table_sql = '
-    CREATE TABLE IF NOT EXISTS `user1` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `utime` int(11) NOT NULL,
-      `username` varchar(255) NOT NULL,
-      `password` varchar(255) NOT NULL,
-      `other1` int(11) NOT NULL,
-      `other2` int(11) NOT NULL,
-      PRIMARY KEY (`id`)
-    ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;';
+// 设置要连接的mysql数据库名
+$db_config['database'] = 'test';
+// 实例化一个mysql连接对象
+$db = new DB($db_config);
 
-    p($db -> query($table_sql));
+// 使用simpleMysql可以执行特殊的sql语句, 通过query方法实现
+$table_sql = '
+CREATE TABLE IF NOT EXISTS `user1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `utime` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `other1` int(11) NOT NULL,
+  `other2` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;';
 
-    $sql = 'select utime, username, password, other1, other2 from user1 where utime > 1496852939 order by utime desc, other1 asc';
-    $arr = $db -> table('user1') -> field('utime, username, password, other1, other2') -> where(array('utime > ' => 1496852939)) -> order('utime desc, other1 asc') -> find();
-    $arr = $db -> query($sql, 'DB::find');
-	p($arr);
+p($db -> query($table_sql));
 
-    $sql = 'delete from user1 where utime>1398736277';
-    $result = $db -> table('user1') -> where(array('utime > ' => 1398736277), array('password' => '')) -> delete();
-    p($result);
-    $sql = 'insert into user1 set(utime, username, password, other1, other2) values ('.time().', "test", "123456", "12312", "21312")';
-    $brr['utime'] = time();
-    $brr['username'] = 'test';
-    $brr['password'] = '123456';
-    $brr['other1'] = '12312';
-    $brr['other2'] = '21312';
-    $brr['test'] = 'test';
-    $result = $db -> table('user1') -> insert($brr);
-    p($result);
-    $sql = 'update user1 set username="change", password="123456" where utime='.$brr['utime'];
-    $brr['username'] = 'change';
-    $brr['password'] = "123'456";
-    $result = $db -> table('user1') -> where(array('utime' => $brr['utime'])) -> update($brr);
-    p($result);
-    $result = $db -> table('user1') -> total();
-    p($result);
-    $result = $db -> table('user1') -> select();
-    p($result);
-    p($db->dbSize());
-}
 
+// 编写自定义sql语句
+$sql = 'select utime, username, password, other1, other2 from user1 where utime > 0 order by utime desc, other1 asc';
+// 执行select查询，如果只需要返回一条记录可以在第二个参数传入DB::find参数；如果需要返回多条记录可以在第二个参数传入DB::select参数
+$arr = $db -> query($sql, 'DB::find');
+// 打印出一维数组
+p($arr);
+
+// 使用类似于phpQuery
+$brr = $db -> table('user1') -> field('utime, username, password, other1, other2') -> where(array('utime > ' => 0)) -> order('utime desc, other1 asc') -> find();
+// 执行select语句将会返回一个二维数组
+p($brr);
+// 执行delete语句将会返回true或false
+$result = $db -> table('user1') -> where(array('utime > ' => 0), array('password' => '')) -> delete();
+p($result);
+
+// 构造一个一维数组作为一条记录插入数据库
+$crr = array();
+$crr['utime'] = time();
+$crr['username'] = 'test';
+$crr['password'] = '123456';
+$crr['other1'] = '12312';
+$crr['other2'] = '21312';
+$crr['test'] = 'test';
+// 执行insert语句将会返回记录自增的ID
+$insert_id = $db -> table('user1') -> insert($crr);
+p($insert_id);
+
+// 对一维数组的字段进行修改, 然后更新到数据库
+$crr['username'] = 'change';
+$crr['password'] = "123'456";
+// 执行update语句将会返回true或false
+$result = $db -> table('user1') -> where(array('utime' => $crr['utime'])) -> update($crr);
+p($result);
+
+// 打印函数
 function p($var)
 {
     echo "<pre>";
